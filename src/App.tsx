92bf39be0f2c3358f074da0ab07535ad52318437
kalz2q <katsu95i@gmail.com>
2022-12-09 15:39:08 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>しりとり</h1>
+      <form>
+        <input type="text" autofocus ></input>
+        <button type="submit" ></button>
+      </form>
     </div>
   );
 }
+const heredoc = `秋の日の
+ヴィオロンの
+かりほの庵の`;
 
 export default App;
